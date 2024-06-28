9e42c1f2f82b7716afa770f8440364cb4f8ca565
enzolp28 <laposta.enzo@gmail.com>
2024-06-28 14:34:47 +0200

    affichage dynamique recettes

--- js/renders.js
+++ js/renders.js
@@ -1,14 +1,36 @@
 function renderRecipes(recipes){
     let html = '';
     recipes.forEach((recipe) => {
+        const {image, name, ingredients, time, description} = recipe;
         html += `
-            <div class="recipe-card">
-                <h3>${recipe.name}</h3>
+            <div class="card">
+                <div class="card-img">
+                    <img src="./img/recipes/${image}" alt="${name}">
+                    <span>${time}min</span>
+                </div>
+                <div class="card-content">
+                    <h3>${name}</h3>
+                    <div class="card-content_recette">
+                        <h4>RECETTE</h4>
+                        <p>${description}.</p>
+                    </div>
+                    <div class="card-content_ingredient">
+                        <h4>INGRÉDIENTS</h4>
+                        <div class="card-ingredient">
+                            ${ingredients.map((ingredient) => `<div class="card-ingredient_list">
+                                <p>${ingredient.ingredient}</p>
+                                <p>${ingredient.quantity} ${ingredient.unit ? ingredient.unit : ''} </p>
+                            </div>`)}
+                        </div>
+                    </div>
+                </div>
             </div>
         `;
     });
 
-    document.querySelector('#recipes-container').innerHTML = html;
+    document.querySelector('.container-card').innerHTML = html;
+
+    document.querySelector('#compteur-recipes').innerText = `${recipes.length} recettes`;
 
 }
 
